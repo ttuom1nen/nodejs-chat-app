d781d5611ab3d07582e210aaab040cd9a99fe87a
ttuom1nen <ttuominen@outlook.com>
2021-04-23 18:45:29 +0300

    Add autoscroll for new messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,11 +13,35 @@
   const locationMessageTemplate = document.querySelector(
     "#location-message-template"
   ).innerHTML;
+  const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
   // Options
   const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
   });
+
+  const autoscroll = () => {
+    // New message element
+    const newMessage = messages.lastElementChild;
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle(newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = messages.scrollHeight;
+
+    // How far the user has scrolled
+    const scrollOffset = messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+      messages.scrollTop = messages.scrollHeight;
+    }
+  };
 
   socket.on("message", (message) => {
     const html = Mustache.render(messageTemplate, {
@@ -26,6 +50,7 @@
       createdAt: moment(message.createdAt).format("HH:mm:ss"),
     });
     messages.insertAdjacentHTML("beforeend", html);
+    autoscroll();
   });
 
   socket.on("locationMessage", (message) => {
@@ -35,6 +60,15 @@
       createdAt: moment(message.createdAt).format("HH:mm:ss"),
     });
     messages.insertAdjacentHTML("beforeend", html);
+    autoscroll();
+  });
+
+  socket.on("roomData", ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+      room,
+      users,
+    });
+    document.querySelector("#sidebar").innerHTML = html;
   });
 
   form.addEventListener("submit", (e) => {
